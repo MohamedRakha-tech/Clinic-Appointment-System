5cc071c828bd8797a8f755a38e32c338bfa239af
wael <ahmed.wael2048@gmail.com>
2026-04-30 22:32:41 +0300

    profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,17 @@ urlpatterns = [
     path("dashboard/doctor/", views.DoctorDashboardView.as_view(), name="doctor_dashboard"),
     path("dashboard/reception/", views.ReceptionDashboardView.as_view(), name="reception_dashboard"),
     path("dashboard/admin/", views.AdminDashboardView.as_view(), name="admin_dashboard"),
+
+    # Profiles
+    path("profile/patient/", views.PatientProfileView.as_view(), name="patient_profile"),
+    path("profile/patient/edit/", views.PatientProfileEditView.as_view(), name="patient_profile_edit"),
+    
+    path("profile/doctor/", views.DoctorProfileView.as_view(), name="doctor_profile"),
+    path("profile/doctor/edit/", views.DoctorProfileEditView.as_view(), name="doctor_profile_edit"),
+    
+    path("profile/reception/", views.ReceptionistProfileView.as_view(), name="reception_profile"),
+    path("profile/reception/edit/", views.ReceptionistProfileEditView.as_view(), name="reception_profile_edit"),
+    
+    path("profile/admin/", views.AdminProfileView.as_view(), name="admin_profile"),
+    path("profile/admin/edit/", views.AdminProfileEditView.as_view(), name="admin_profile_edit"),
 ]
